a8a86d391175f0aa4335f4242cd90e96b6cbe6cd
jpike97 <jpike@sagepath.com>
2022-01-20 11:20:50 -0500

    first commit

---
+++ scripts/editMapData.js
@@ -0,0 +1,33 @@
+
+//weird stuff
+//simplemaps_usmap_mapdata.main_settings.state_description = get;
+
+console.log(timeZoneGDPJson);
+console.log(stateGDPJson);
+let dollarUS = Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0
+});
+
+function initEditData() { 
+    for (var key in stateGDPJson) {
+        if (stateGDPJson.hasOwnProperty(key)) {
+            fillStateDesc(key, stateGDPJson[key]);
+        }
+    }
+}
+
+initEditData();
+
+function fillStateDesc(stateName, stateGDP) { 
+
+    let state = statesTitleCase.find(el => el.name === stateName);
+    stateGDP = dollarUS.format(stateGDP);
+    if (state != undefined) { 
+        console.log("state Abbrv is " + state.abbreviation);
+        simplemaps_usmap_mapdata.state_specific[state.abbreviation]["description"] = "GDP (in millions) " + stateGDP;
+    }
+    
+}
---
+++ scripts/mapdata.js
@@ -0,0 +1,413 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "650", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    
+    //Label defaults
+    label_color: "#002767",
+    hide_labels: "no",
+    border_color: "white",
+    
+    //State defaults
+    state_description: "Learn about the products available in your state.",
+    state_color: "#77bcff",
+    state_hover_color: "#038EEB",
+    state_url: "http://simplemaps.com",
+    all_states_inactive: "no",
+    
+    //Location defaults
+    location_description: "Field Office",
+    location_color: "#ff46a2",
+    location_pulse: "yes",
+    location_pulse_size: "3",
+    location_opacity: ".8",
+    location_url: "http://simplemaps.com",
+    all_states_zoomable: "yes",
+    location_size: "40",
+    location_type: "marker",
+    all_locations_inactive: "no",
+    url_new_tab: "yes",
+    auto_load: "yes",
+   
+    //Zoom settings
+    zoom: "yes",
+    initial_zoom: "-1",
+    initial_zoom_solo: "yes",
+    
+    //Advanced settings
+    div: "map",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    label_hover_color: "",
+    label_size: "",
+    label_font: "",
+    popups: "detect"
+  },
+  state_specific: {
+    HI: {
+      name: "Hawaii",
+      hide: "no",
+      inactive: "no"
+    },
+    AK: {
+      name: "Alaska",
+      hide: "no",
+      inactive: "no"
+    },
+    FL: {
+      name: "Florida",
+      inactive: "no",
+      hide: "no"
+    },
+    NH: {
+      name: "New Hampshire",
+      hide: "no",
+      inactive: "no"
+    },
+    VT: {
+      name: "Vermont",
+      hide: "no",
+      inactive: "no"
+    },
+    ME: {
+      name: "Maine",
+      hide: "no",
+      inactive: "no"
+    },
+    RI: {
+      name: "Rhode Island",
+      hide: "no",
+      inactive: "no"
+    },
+    NY: {
+      name: "New York",
+      hide: "no",
+      inactive: "no"
+    },
+    PA: {
+      name: "Pennsylvania",
+      hide: "no",
+      inactive: "no"
+    },
+    NJ: {
+      name: "New Jersey",
+      hide: "no",
+      inactive: "no"
+    },
+    DE: {
+      name: "Delaware",
+      hide: "no",
+      inactive: "no"
+    },
+    MD: {
+      name: "Maryland",
+      hide: "no",
+      inactive: "no"
+    },
+    VA: {
+      name: "Virginia",
+      hide: "no",
+      inactive: "no"
+    },
+    WV: {
+      name: "West Virginia",
+      hide: "no",
+      inactive: "no"
+    },
+    OH: {
+      name: "Ohio",
+      hide: "no",
+      inactive: "no"
+    },
+    IN: {
+      name: "Indiana",
+      hide: "no",
+      inactive: "no"
+    },
+    IL: {
+      name: "Illinois",
+      hide: "no",
+      inactive: "no"
+    },
+    CT: {
+      name: "Connecticut",
+      hide: "no",
+      inactive: "no"
+    },
+    WI: {
+      name: "Wisconsin",
+      hide: "no",
+      inactive: "no"
+    },
+    NC: {
+      name: "North Carolina",
+      hide: "no",
+      inactive: "no"
+    },
+    DC: {
+      name: "District of Columbia",
+      hide: "no",
+      inactive: "no"
+    },
+    MA: {
+      name: "Massachusetts",
+      hide: "no",
+      inactive: "no"
+    },
+    TN: {
+      name: "Tennessee",
+      hide: "no",
+      inactive: "no"
+    },
+    AR: {
+      name: "Arkansas",
+      hide: "no",
+      inactive: "no"
+    },
+    MO: {
+      name: "Missouri",
+      hide: "no",
+      inactive: "no"
+    },
+    GA: {
+      name: "Georgia",
+      hide: "no",
+      inactive: "no"
+    },
+    SC: {
+      name: "South Carolina",
+      hide: "no",
+      inactive: "no"
+    },
+    KY: {
+      name: "Kentucky",
+      hide: "no",
+      inactive: "no"
+    },
+    AL: {
+      name: "Alabama",
+      hide: "no",
+      inactive: "no"
+    },
+    LA: {
+      name: "Louisiana",
+      hide: "no",
+      inactive: "no"
+    },
+    MS: {
+      name: "Mississippi",
+      hide: "no",
+      inactive: "no"
+    },
+    IA: {
+      name: "Iowa",
+      hide: "no",
+      inactive: "no"
+    },
+    MN: {
+      name: "Minnesota",
+      hide: "no",
+      inactive: "no"
+    },
+    OK: {
+      name: "Oklahoma",
+      hide: "no",
+      inactive: "no"
+    },
+    TX: {
+      name: "Texas",
+      hide: "no",
+      inactive: "no"
+    },
+    NM: {
+      name: "New Mexico",
+      hide: "no",
+      inactive: "no"
+    },
+    KS: {
+      name: "Kansas",
+      hide: "no",
+      inactive: "no"
+    },
+    NE: {
+      name: "Nebraska",
+      hide: "no",
+      inactive: "no"
+    },
+    SD: {
+      name: "South Dakota",
+      hide: "no",
+      inactive: "no"
+    },
+    ND: {
+      name: "North Dakota",
+      hide: "no",
+      inactive: "no"
+    },
+    WY: {
+      name: "Wyoming",
+      hide: "no",
+      inactive: "no"
+    },
+    MT: {
+      name: "Montana",
+      hide: "no",
+      inactive: "no"
+    },
+    CO: {
+      name: "Colorado",
+      hide: "no",
+      inactive: "no"
+    },
+    UT: {
+      name: "Utah",
+      hide: "no",
+      inactive: "no"
+    },
+    AZ: {
+      name: "Arizona",
+      hide: "no",
+      inactive: "no"
+    },
+    NV: {
+      name: "Nevada",
+      hide: "no",
+      inactive: "no"
+    },
+    OR: {
+      name: "Oregon",
+      hide: "no",
+      inactive: "no"
+    },
+    WA: {
+      name: "Washington",
+      hide: "no",
+      inactive: "no"
+    },
+    CA: {
+      name: "California",
+      hide: "no",
+      inactive: "no"
+    },
+    MI: {
+      name: "Michigan",
+      hide: "no",
+      inactive: "no"
+    },
+    ID: {
+      name: "Idaho",
+      hide: "no",
+      inactive: "no"
+    },
+    GU: {
+      name: "Guam",
+      hide: "yes",
+      inactive: "no"
+    },
+    VI: {
+      name: "Virgin Islands",
+      hide: "yes",
+      inactive: "no"
+    },
+    PR: {
+      name: "Puerto Rico",
+      hide: "yes",
+      inactive: "no"
+    },
+    MP: {
+      name: "Northern Mariana Islands",
+      hide: "yes",
+      inactive: "no"
+    }
+  },
+  locations: {
+  },
+  regions: {
+    "0": {
+      states: [
+        "GA",
+        "SC",
+        "NC",
+        "VA",
+        "PA",
+        "WV",
+        "VT",
+        "NJ",
+        "DE",
+        "DC",
+        "MD",
+        "RI",
+        "CT",
+        "MA",
+        "NH",
+        "NY",
+        "ME",
+        "FL",
+        "OH",
+        "IN",
+        "MI",
+        "KY",
+        "TN"
+      ],
+      name: "Eastern Time Zone",
+      color: "Green",
+      description: "GDP (in Millions) $10,194,921"
+    },
+    "1": {
+      states: [
+        "WI",
+        "IL",
+        "MO",
+        "MS",
+        "AL",
+        "AR",
+        "LA",
+        "TX",
+        "OK",
+        "KS",
+        "MO",
+        "IA",
+        "NE",
+        "SD",
+        "ND",
+        "MN"
+      ],
+      name: "Central Time Zone",
+      color: "Red",
+      description: "GDP (in Millions) $5,182,247"
+    },
+    "2": {
+      states: [
+        "MT",
+        "WY",
+        "CO",
+        "NM",
+        "AZ",
+        "UT",
+        "ID",
+        "MT"
+      ],
+      name: "Mountain Time Zone",
+      color: "Yellow",
+      description: "GDP (in Millions) $1,223,988"
+    },
+    "3": {
+      states: [
+        "WA",
+        "OR",
+        "CA",
+        "NV"
+      ],
+      name: "Pacific Time Zone",
+      color: "Blue",
+      description: "GDP (in Millions) $4,026,159"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  }
+};
